94af8e177e346b67c4b326ae9abaf522e1fc01bf
Marc Held <marc@leviathan.ai>
2020-03-22 10:32:17 -0400

    adding corpus to spacy so we can extract stocks!

--- anal.py
+++ anal.py
@@ -1,21 +1,44 @@
 import praw
 import os
+import csv
 import spacy
+from re import sub
 from dotenv import load_dotenv
 load_dotenv()
+
+def kebab(s):
+  return sub(
+    r"(\s|_|-)+","-",
+    sub(
+      r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
+      lambda mo: mo.group(0).lower(), s))
 
 reddit = praw.Reddit(client_id=os.getenv('client_id'),
     client_secret=os.getenv('client_secret'),
     password=os.getenv('password'),
     username=os.getenv('username'),
     user_agent='wsb-anal v1')
-# don't want to actually be able to post anything
 reddit.read_only = True
-
 wsb = reddit.subreddit('wallstreetbets')
 
 # python -m spacy download en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
+ruler = spacy.pipeline.EntityRuler(nlp)
+
+patterns = []
+# ticker, company_name, short_name, industry, description,
+# website, logo, ceo, exchange, market cap, sector, tag_1, tag_2, tag_3
+with open('companies.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        patterns.append({
+            "label": "STOCK", 
+            "pattern":row["ticker"], 
+            "id":kebab(row["short_name"])})
+
+ruler.add_patterns(patterns)
+nlp.add_pipe(ruler)
+
 
 def combined(submission):
     return submission.title + " " + submission.selftext
